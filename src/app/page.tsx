7210dd8031e680ed3e5bf137d9057147bc75d133
Ricardo Herrera <rickhl@outlook.com>
2025-11-22 21:10:10 -0500

    fix: link to resume pdf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -234,11 +234,11 @@ function Resume() {
         ))}
       </ol>
       <Button
-        href="/ricardo.pdf"
+        href="/Ricardo_Herrera_Resume.pdf"
         variant="secondary"
         className="group mt-6 w-full"
       >
-        Download CV
+        Download Resume
         <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
       </Button>
     </div>
