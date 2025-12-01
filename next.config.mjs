98c3666bb9f802564ec333f9c1cd7ece00b97817
Ricardo Herrera <rickhl@outlook.com>
2025-12-01 09:05:53 -0500

    chore: upgrade nextjs to v16

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
-import rehypePrism from '@mapbox/rehype-prism'
-import nextMDX from '@next/mdx'
-import remarkGfm from 'remark-gfm'
+import createMDX from '@next/mdx'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -10,11 +8,10 @@ const nextConfig = {
   },
 }
 
-const withMDX = nextMDX({
-  extension: /\.mdx?$/,
+const withMDX = createMDX({
   options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [rehypePrism],
+    remarkPlugins: ['remark-gfm'],
+    rehypePlugins: ['@mapbox/rehype-prism'],
   },
 })
 
